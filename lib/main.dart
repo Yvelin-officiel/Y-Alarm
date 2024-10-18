771a98ab657557db3b1afabc9d631d816f195c8d
unknown <gerbeaudy@yahoo.fr>
2024-10-18 10:19:35 +0200

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'widgets/infos_fenetre.dart';
 
-import 'package:y_alarm/screens/alarm.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -14,13 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Y-alarm',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // press "r" if you used the command line to hot reload the app.
-        // To reset the state, use hot restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
@@ -32,14 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,12 +34,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void alarm() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const Alarm()),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,12 +41,8 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('Accueil'),
       ),
-      body: ElevatedButton(
-        child: Text('Bienvenue sur la page alarme'),
-        onPressed: () {
-          alarm();
-        },
-      ),
+      body: Expanded(child: GetFenetre()),
+
     );
   }
     
